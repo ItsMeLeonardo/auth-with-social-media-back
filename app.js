45e490f02fb59a845c6f37aff02ec010b58deee1
Carlos Leonardao <74982565+ItsMeLeonardo@users.noreply.github.com>
2022-01-02 12:05:46 -0500

    [Migrate] from Mongo Atlas to CosmosDB

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const cors = require("cors");
 const UserRouter = require("./controllers/User");
 const LoginRouter = require("./controllers/Login");
-const notFount = require("./middleware/notFound");
+const notFount = require("./middlewares/notFound");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -18,7 +18,7 @@ app.use("/api/users", UserRouter);
 app.use("/api/auth", LoginRouter);
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello World!" });
+  res.json({ message: "Hello World! from azure + MongoDB" });
 });
 
 // unknown route
